a02a01e4babdb66e9e5fd92aac104141b98b30b1
leoalvesviana <leonardo.viana@meta3.com.br>
2021-12-01 08:23:20 -0300

    ok

--- src/content/applications/DetailsCliente/PageHeader.tsx
+++ src/content/applications/DetailsCliente/PageHeader.tsx
@@ -1,14 +1,14 @@
-import { Typography, Button, Grid } from '@mui/material';
+import { Typography, Button, Grid, CardContent, Card } from '@mui/material';
 import Modals from 'src/content/pages/Components/Modals';
 import AddTwoToneIcon from '@mui/icons-material/AddTwoTone';
 import { useEffect, useState } from 'react';
 import api from 'src/service/api';
 
-interface headerprops{
+interface headerprops {
   codigoCli: number;
 }
 
-function PageHeader<headerprops>({codigoCli}) {
+function PageHeader<headerprops>({ codigoCli }) {
 
   const [clienteD, setClienteD] = useState<any>();
 
@@ -28,35 +28,38 @@ function PageHeader<headerprops>({codigoCli}) {
     avatar: '/static/images/avatars/1.jpg'
   };
   return (
-    <Grid container justifyContent="space-between" alignItems="center">
-      <Grid item>
-        <Typography variant="h3" component="h3" gutterBottom>
-          Cliente
-        </Typography>
-        {clienteD &&
-          <Typography variant="subtitle2">
-          Cliente: {clienteD.cliente.nome}<br />
-          Email: {clienteD.cliente.email1}<br />
-          Observações: {clienteD.cliente.observacao}
-        </Typography>
-        }
-      </Grid>
-      <Grid item>
-        <Typography variant="h3" component="h3" gutterBottom>
-          Resumo
-        </Typography>
-        {clienteD &&
-          <Typography variant="subtitle2">
-          Resumo do cliente: {clienteD.numCompras}<br />
-          Numero de compras: {clienteD.numItens}<br />
-          Total geral: {clienteD.totalGeral}
-        </Typography>
-        }
-      </Grid>
-      <Grid item>
-        <Modals />
-      </Grid>
-    </Grid>
+    <Card>
+      <CardContent>
+        <Grid container justifyContent="space-between" alignItems="center">
+          <Grid item>
+            <Typography variant="h3" component="h3" gutterBottom>
+              Cliente
+            </Typography>
+            {clienteD &&
+              <Typography variant="subtitle2">
+                Cliente: {clienteD.cliente.nome}<br />
+                Email: {clienteD.cliente.email1}<br />
+                Observações: {clienteD.cliente.observacao}
+              </Typography>
+            }
+          </Grid>
+          <Grid item>
+            <Typography variant="h3" component="h3" gutterBottom>
+              Resumo
+            </Typography>
+            {clienteD &&
+              <Typography variant="subtitle2">
+                Numero de Compras: {clienteD.numCompras}<br />
+                Numero de Itens: {clienteD.numItens}<br />
+                Total geral: {clienteD.totalGeral}
+              </Typography>
+            }
+          </Grid>
+          <Grid item>
+          </Grid>
+        </Grid>
+      </CardContent>
+    </Card>
   );
 }
 
